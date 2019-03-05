dc9daa244c0f9dd36c2b200c67bab3a81554255f
Abhinav Singh <adityasaha890@gmail.com>
2019-03-06 00:21:14 +0530

    H

--- sockets/socket_fucn.js
+++ sockets/socket_fucn.js
@@ -4,8 +4,8 @@ const {perma_order,temp_order,perma}=require('../database/db');
 
 var io;
 var connected_socket;
+
 function connection(port){
-    //console.log(port);
     io=socket(port);
     io.on('connection',socket=>{
         connected_socket=socket;
@@ -15,7 +15,6 @@ function connection(port){
             io.sockets.emit("new_delivery_request",req);
         });
         connected_socket.on("request_accepted_bydriver",(data)=>{
-            console.log(data);
             perma.findById({_id:data.User_id},{$pull:{'temp_History':{'Order_id':data._id}}},{$addToSet:{'perma_History':{'Order_id':data._id}}},{new:true}).then(user=>{
                 temp_order.findByIdAndDelete({_id:data._id}).then(user=>{
                     const db=new perma_order
@@ -31,7 +30,7 @@ function connection(port){
                     db.Price=req.body.Price,
                     db.Date=new Date();
                     db.save().then(user=>{
-                        console.log(user);
+                        console.log("33 socket_fucn.js"+user);
                     }).catch(err=>{console.log(err)});
                 })
             })
