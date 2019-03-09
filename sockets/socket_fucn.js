bce4247f2a8f9f4e0899c9f788937fbbc9b8c6cc
Abhinav Singh <adityasaha890@gmail.com>
2019-03-09 10:23:59 +0530

    h

--- sockets/socket_fucn.js
+++ sockets/socket_fucn.js
@@ -22,7 +22,7 @@ function connection(port){
             {$set:{'History.$.CurrentStatus':1}},{new:true},(err,result)=>{
                 if(result){
                     order.findByIdAndUpdate({_id:data.data._id},{CurrentStatus:1}).then(user=>{
-                        socket.emit("successfully accpeted",data.sender_unique);
+                        io.socket.emit("successfully accpeted",data.sender_unique);
                     }).catch(err=>console.log("26 socket_fucn"+err))
                 }
                 else if(err)
