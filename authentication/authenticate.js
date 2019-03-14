9d7daa14add35d15a04ffd77c38f7a42a86ae53c
Abhinav Singh <adityasaha890@gmail.com>
2019-03-15 01:55:06 +0530

    G

--- authentication/authenticate.js
+++ authentication/authenticate.js
@@ -20,7 +20,8 @@ const get_token=(req,res,next)=>{
         req.token=token.split(' ')[1];
         next();
     }
-    res.status(401).json({err:"Not authorised"});
+    else
+        res.status(401).json({err:"Not authorised"});
 }
 
 const transporter= nodemailer.createTransport({
@@ -234,7 +235,7 @@ router.get('/resetpass',get_token,(req,res)=>{
 router.post('/ressetingdone/:token',(req,res)=>{
     jwt.verify(req.params.token,"suab",(err,authdata)=>{
         perma.findOneAndUpdate({Email:authdata.user},{Password:req.body.password},{new:true}).then(user=>{
-            res.status(200).json(user);
+            res.status(200).json({msg:"Successfully changed"});
         }).catch(err=>{res.send(req.params.email)})
     })
 })
