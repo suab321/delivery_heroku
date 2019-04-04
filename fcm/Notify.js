c9cacd37e4664add34d6c26a3af55ffc7be708ad
Abhinav Singh <adityasaha890@gmail.com>
2019-04-05 01:21:29 +0530

    H

--- fcm/Notify.js
+++ fcm/Notify.js
@@ -1,13 +1,12 @@
 var FCM=require('fcm-push');
-var serverkey=process.env.SERVER_KEY;
+var serverkey="AIzaSyCgJqVv7yZ97gcOoADX8uaCTFEeuiqbK2Y";
 var fcm = new FCM(serverkey);
 
 function notify(users){
     console.log(users)
-    console.log(process.env.SERVER_KEY)
     var message={
         to:"device_id",
-        collapse_key:process.env.COLLAPSE_KEY,
+        collapse_key:"e1wOospPHEU:APA91bGBJ99mYTkpWwDRPsiyEojSZVyASyw_MI-zygf9N-A5_EIbp-W9zlu-iFubosWmhvvUZrLiBwzFUU69PANhDja7j8zUZ1XK5mwe17R-Gd3b4Ah4Q01izyPd698NFicG7izIdJXG",
         notification:{
             title:"Title of notification",
             body:"Body of notification"
