b92616520076ef460567f914af422c8d8285d54b
Abhinav Singh <adityasaha890@gmail.com>
2019-04-13 22:05:02 +0530

    g

--- authentication/authenticate.js
+++ authentication/authenticate.js
@@ -185,9 +185,6 @@ router.post('/login',(req,res)=>{
                 perma.findById({_id:user.id},{Password:false}).then(user=>{
                     req.session.user=user._id;
                     const enct=token.generateToken(user._id);
-                    perma.findById({_id:user.id},{device_id:req.body.device_id},{new:true}).then(user=>{
-                        console.log
-                    })
                    res.status(200).json({key:enct,response:"1"});
                 })
             }
