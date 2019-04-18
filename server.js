119ac3d0179227fbd19e476220581910be9c403b
Abhinav Singh <adityasaha890@gmail.com>
2019-04-18 14:51:49 +0530

    G

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ const verify=(req,res,next)=>{
 app.get('/pay_for_service1',(req,res)=>{
     console.log(req.query);
             price.find({}).then(user=>{
-                res.render('payment',{order_id:req.query.order_id,weight:req.query.weight,charge:5,stripePublicKey:publicKey})
+                res.render('payment',{order_id:req.query.order_id,weight:req.query.weight,charge:25,stripePublicKey:publicKey})
             }).catch(err=>{
                 console.log(err)
             })
